5005fd5f30d9353d460e8563bac2661b0812e996
Matt <matt@mattsphotos.com.au>
2016-10-21 16:23:09 +1100

    Create controller.rb

---
+++ controller.rb
@@ -0,0 +1,3 @@
+response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+response.headers["Pragma"] = "no-cache"
+response.headers["Expires"] = "Fri, 01 Jan 2016 00:00:00 GMT"
